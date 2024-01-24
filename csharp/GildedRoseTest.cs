4ac8502323fdd5290f072d60b3af5a1645cc52a3
if21b258 <if21b258@technikum-wien.at>
2024-01-24 20:07:22 +0100

    test Quality restrictions and Aged Brie interaction

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -22,5 +22,61 @@ namespace csharp
             Assert.AreEqual(9, items[0].SellIn);
             Assert.AreEqual(9, items[0].Quality);
         }
+
+        [Test]
+        public void QualityDegradesTwiceAsFastAfterSellByDate()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Item", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(8, items[0].Quality);
+        }
+
+        [Test]
+        public void QualityIsNeverNegative()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Item", SellIn = 10, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
+        [Test]
+        public void AgedBrieIncreasesInQuality()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(11, items[0].Quality);
+        }
+
+        [Test]
+        public void QualityIsNeverMoreThan50()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(50, items[0].Quality);
+        }
     }
 }
